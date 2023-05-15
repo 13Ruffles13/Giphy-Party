a9e31699677c7f80bc32c0aed6e0f59eb46e11ea
13Ruffles13 <87215494+13Ruffles13@users.noreply.github.com>
2023-05-14 17:34:15 -0700

    GiphyProject
    Uploaded files!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Giphy Party
- Giphy API using AJAX request.
+
+Giphy API using AJAX request.
+
+- Allows the user to search for a GIF and when the form is submitted, program makes a AJAX request to the Giphy API and returns a single GIF
+- Once the Giphy API has responded with data, appends the GIF to the page
+- Allows the user to search for as many GIFs as they would like and keep appending them to the page
+- Allows the user to remove all of the GIFs by clicking a button
---
+++ app.js
@@ -0,0 +1,42 @@
+//Element locations
+const $searchText = $("#userInput");
+const $memeBucket = $(".memeContainer");
+
+//Event Handlers
+const $searchGif = $("#searchGif").on("click", findGif);
+const $removeGif = $("#removeGif").on("click", removeAllGifs);
+
+//API REQUESTS
+async function findGif(e) {
+  e.preventDefault();
+  //Base case for search input
+  if ($searchText.val() == "") return alert("Please enter a search");
+
+  let searchVal = $("#userInput").val();
+  const res = await axios.get("https://api.giphy.com/v1/gifs/search", {
+    params: { q: searchVal, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" },
+  });
+
+  //Add Images to memeContainer
+  addGif(res.data);
+
+  //Clear settings
+  $searchText.val("");
+}
+
+//Function handlers
+function addGif(res) {
+  let dataLength = res.data.length;
+  let pickRandom = Math.floor(Math.random() * dataLength);
+  let $newImg = $("<img>", {
+    src: res.data[pickRandom].images.original.url,
+    alt: $searchText.val(),
+    className: "memeContainer",
+  });
+
+  $memeBucket.append($newImg);
+}
+function removeAllGifs(e) {
+  $(".memeContainer").empty();
+}
+console.log("Let's get this party started!");
